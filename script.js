376a07fa1b0ae115ab9e28813861a723b49cebe0
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2024-04-07 10:22:20 +0200

    link style

--- script.js
+++ script.js
@@ -186,35 +186,26 @@ document.querySelector('.btn-download').addEventListener('click', (e) => {
     downloadDashboardAsPDF();
 });
 
-// SIDE MENU
+//Navbar
 
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
 });
-
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
-
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
-
-const switchMode = document.getElementById('switch-mode');
-
-switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-	}
-})
